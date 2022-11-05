e69e8e86da3026efc3be0383f59f600ac9f0d704
Michael Heinzl <michael.heinzl@me.com>
2022-11-05 13:08:44 +0100

    Use EmptyReducer in CompactMap tests

--- Tests/ComposableNavigationTests/StoreCompactMapTest.swift
+++ Tests/ComposableNavigationTests/StoreCompactMapTest.swift
@@ -4,7 +4,7 @@ import ComposableArchitecture
 
 class StoreCompactMapTest: XCTestCase {
 	func testCompactMapNonOptional() throws {
-		let store = Store<Int?, Void>(initialState: 12, reducer: Reducer.empty, environment: ())
+		let store = Store<Int?, Void>(initialState: 12, reducer: EmptyReducer())
 		let compactMappedStore = try XCTUnwrap(
 			store.compactMap({ $0.scope(state: { $0 * 2 }) }),
 			"Store should not be nil after compactMap"
@@ -13,7 +13,7 @@ class StoreCompactMapTest: XCTestCase {
 	}
 	
 	func testCompactMapOptional() throws {
-		let store = Store<Int?, Void>(initialState: nil, reducer: Reducer.empty, environment: ())
+		let store = Store<Int?, Void>(initialState: nil, reducer: EmptyReducer())
 		let compactMappedStore = store.compactMap({ $0.scope(state: { $0 * 2 }) })
 		XCTAssertNil(compactMappedStore)
 	}
